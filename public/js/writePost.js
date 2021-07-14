1faa79788cbd68420c186c17de3ea4bee5648ea6
Peter Roto <peterdroto@gmail.com>
2021-07-14 17:02:39 -0500

    fixed write post again

--- public/js/writePost.js
+++ public/js/writePost.js
@@ -1,8 +1,9 @@
+const subPost = document.querySelector('#addPost');
 async function newFormHandler(event) {
     event.preventDefault();
-
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
+    const post_content = document.querySelector('textarea').value;
+    // const post_content = document.querySelector('#post-content').value;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
@@ -21,5 +22,4 @@ async function newFormHandler(event) {
         alert(response.statusText);
     }
 }
-
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+subPost.addEventListener('click', newFormHandler);
